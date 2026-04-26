f16d7e4f6ddf2b08a2fb635e8c85dbdd47ae44b9
Heitor <mafaldoheitor@gmail.com>
2026-04-26 10:07:31 -0300

    Seção de background color

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,6 +5,26 @@ function Main(){
 
     const [valorGeral,setValorGeral] = useState(0)
 
+    const [redColor,setRedColor] = useState(0)
+    const [greenColor,setGreenColor] = useState(0)
+    const [blueColor,setBlueColor] = useState(0)
+
+    function addColorRed(){
+        setRedColor(redColor => redColor + 1)
+    }
+
+    function addColorGreen(){
+        setGreenColor(greenColor => greenColor + 1)
+    }
+
+    function addColorBlue(){
+        setBlueColor(blueColor => blueColor + 1)
+    }
+
+    function addColorRed(){
+        setRedColor(redColor => redColor + 1)
+    }
+
     function addValue(){
         setValorGeral(valorGeral => valorGeral + 1)
     }
@@ -34,17 +54,19 @@ function Main(){
                 </div>
                 <div className="square" style={{borderRadius: valorGeral}}>
                 </div>
-
-
                 </section>
                 <section id="box-shadow">
-
+                
                 </section>
                 <section id="margin">
 
                 </section>
                 <section id="padding">
 
+                </section>
+                <section id="background-color" className="section-background-color ">
+                    <div className="squar2" style={{backgroundColor: redColor}}></div>
+                    <button onClick={addColorRed}>Red Color</button>
                 </section>
             </div>
         </>
