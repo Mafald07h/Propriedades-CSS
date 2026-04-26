ec3b305f5e7c07d8be6ef021dccfc9ea0be77bf5
Heitor <mafaldoheitor@gmail.com>
2026-04-26 09:29:49 -0300

    Criar componente Main

---
+++ src/components/Main.jsx
@@ -0,0 +1,37 @@
+import "./Main.css"
+
+function Main(){
+    return (
+        <>
+            <div className="container">
+                <section id="border-radius">
+
+
+
+
+
+
+
+
+
+
+                </section>
+
+
+
+
+                <section id="box-shadow">
+
+                </section>
+                <section id="margin">
+
+                </section>
+                <section id="padding">
+
+                </section>
+            </div>
+        </>
+    )
+}
+
+export default Main
