56f4006339ac641051338ef813fa97d4d2171ed6
Heitor <mafaldoheitor@gmail.com>
2026-04-26 09:26:34 -0300

    Adicionar menu de navegação para poder navegar pelas propriedades

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css"
+
+function Header(){
+    return(
+        <>
+        <header>
+            <h1>Propriedades CSS aplicadas em tempo real</h1>
+            <nav>
+                <a href="#border-radius">Border-Radius</a>
+                <a href="#box-shadow">Box-shadow</a>
+                <a href="#margin">Margin</a>
+                <a href="#padding">Padding</a>
+            </nav>
+        </header>
+        </>
+    )
+}
+
+export default Header
